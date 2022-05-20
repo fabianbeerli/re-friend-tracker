c35dfcafab5bb506f1828d0999f6af86198c7e74
fabianbeerli <faebebee@gmail.com>
2022-05-20 13:45:03 +0000

    Commit for deployment

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -457,7 +457,7 @@ export class GuiModel {
                         "form": {
                         "form": "AddActivityForm"
                         }
-                        },
+                        }, 
                     ]
                    }
                    ,
